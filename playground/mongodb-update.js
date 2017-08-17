a83c1a981cc6e5bc249cc821a250915ec443b9d9
Woojae Jun <woojaeizzle@gmail.com>
2017-08-17 10:57:34 +0900

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err)
+		return console.log('Cannot connect to MondoDB server.');
+	console.log('Connected');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5994eb91b70f9427369b7260')}, 
+	// 	{
+	// 		$set: {completed: true}
+	// 	}, {
+	// 		returnOriginal: false
+	// 	}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5994e4bbb70f9427369b7143')
+	}, {
+		$set: {
+			location: 'Hopkinsville'
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	db.close();
+});
