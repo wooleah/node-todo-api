7645efbb72002b33d69f646020ba97e29e2790b6
Woojae Jun <woojaeizzle@gmail.com>
2017-08-20 17:02:01 +0900

    Added some comment while reviewing

--- server/server.js
+++ server/server.js
@@ -1,24 +1,31 @@
 var express = require('express');
+//body-parser lets us take json and send it to server
+//takes body and turns(parse) it into javascript object
 //body-parser takes your json and convert it to an object + attaching it to req object
 var bodyParser = require('body-parser');
 
 //calling mongoose property in an object(exported), using destructuring
+//if the var name and property name matches
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json()); //return value is the function we need
+//configuring middleware(body-parser)
+app.use(bodyParser.json()); //return value is the function we need to give to express
 
 //POST - creating new todos
 app.post('/todos', (req, res) => {
+	// console.log(req.body); //you need body-parser to do this
 	var todo = new Todo({
 		text: req.body.text
 	});
-	todo.save().then((doc) => {
+	todo.save().then((doc) => { //success case
+	//save()->change is saved
+	//Mongoose async operations like .save() returns Promises
 		res.send(doc);
-	}, (err) => {
+	}, (err) => { //error case
 		res.status(400).send(err);
 	});
 });
@@ -26,3 +33,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+
+module.exports = {app}; //app: app->shortened ES6
+//or
+//module.exports.app = app;
